03fc8bcca6cbc0280d78b5e7f46adb8aae7da76d
Ivan Komar <ikomar@egp.com.ua>
2020-05-03 00:21:26 +0300

    add vue-chartjs and implement history PieChart

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,9 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <canvas ref="canvas">
+       
+      </canvas>
     </div>
 
     <Loader v-if="loading"/>
@@ -38,8 +40,12 @@
 <script>
 import HistoryTable from '@/components/HistoryTable'
 import paginationMixin from '@/mixins/pagination.mixin'
+import currencyFilter from '@/filters/currency.filter'
+
+import { Pie } from 'vue-chartjs'
 export default {
   name: 'history',
+  extends: Pie,
   mixins: [paginationMixin],
   components: {
     HistoryTable
@@ -48,18 +54,56 @@ export default {
     loading: true,
     records: [],
   }),
+  methods: {
+    setup(categories) {
+      this.setupPagination(this.records.map(record => {
+        return {
+          ...record,
+          categoryName: categories.find(c => c.id === record.categoryId).title,
+          typeClass: record.type === 'income' ? 'green' : 'red',
+          typeText: record.type === 'income' ? 'Доход' : 'Расход'
+        }
+      }).reverse())
+
+      this.renderChart({
+        labels: categories.map(c => c.title),
+        datasets: [{
+            label: 'Расходы по категориям',
+            data: categories.map( c => {
+              return this.records.reduce( (total, rec) => {
+                if (rec.categoryId === c.id && rec.type === 'outcome') {
+                  total += +rec.amount
+                }
+                return total
+              }, 0)
+            }),
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+      })
+    },
+  },
   async mounted() {
     this.records = await this.$store.dispatch('fetchRecords')
     const categories = await this.$store.dispatch('fetchCategories')
 
-    this.setupPagination(this.records.map(record => {
-      return {
-        ...record,
-        categoryName: categories.find(c => c.id === record.categoryId).title,
-        typeClass: record.type === 'income' ? 'green' : 'red',
-        typeText: record.type === 'income' ? 'Доход' : 'Расход'
-      }
-    }).reverse())
+    this.setup(categories)
+
     this.loading = false
   },
  
